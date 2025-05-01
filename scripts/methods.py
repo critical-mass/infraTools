d110fe615c9d45671015166a03590d3b6db8b161
Daniel Thompson <thompson9451@gmail.com>
2025-05-01 16:29:55 -0400

    backing up python methods

---
+++ scripts/methods.py
@@ -0,0 +1,47 @@
+import json
+import requests
+
+
+class okta:
+    def __init__(self, user_id, base_url, headers, group_id):
+        self.user_id = user_id
+        self.base_url = base_url
+        self.headers = headers
+        self.group_id = group_id
+        self.description = description
+        self.create_group = group_name
+
+
+    def read_user(user_id, base_url, headers):
+        url = f"{base_url}/api/v1/users/{user_id}"
+        payload = {}
+        response = requests.get(url, headers=headers, data=payload)
+        return(response)
+
+    def activate_user(user_id, base_url, headers):
+        url = f"{base_url}/api/v1/users/{user_id}/lifecycle/activate?sendEmail=true"
+        payload = {}
+        response = requests.post(url, headers=headers, data=payload)
+        return(response)
+
+    def deactivate_user(user_id, base_url, headers):
+        url = f"{base_url}/api/v1/users/{user_id}/lifecycle/deactivate"
+        payload = {}
+        response = requests.post(url, headers=headers, data=payload)
+        return(response)
+
+    def add_user_to_group(user_id, base_url, headers, group_id):
+        url = f"{base_url}/api/v1/groups/{group_id}/users/{user_id}"
+        payload = {}
+        response = requests.post(url, headers=headers, data=payload)
+        return(response)
+
+    def create_group(base_url, group_name, description, headers):
+        url = f"{base_url}/api/v1/groups/"
+        payload = f{
+            "profile": {
+                "description": description
+                "name": group_name
+            }
+        }
+        response = requests.post(url, headers=headers, data=payload)
